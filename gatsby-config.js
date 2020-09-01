a65bbb6172866cead821ca92e6f3b9892c59bf42
fourside <fourside@gmail.com>
2020-09-01 09:00:19 +0900

    modify profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `fourside.github.io`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `fourside`,
+      summary: `programmer`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `programmer's note`,
+    siteUrl: `https://fourside.github.io/`,
   },
   plugins: [
     {
@@ -60,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `fourside.github.io`,
+        short_name: `fourside`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
